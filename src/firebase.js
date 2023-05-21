095e18f55f1e2cf55f6d1079c52075bfda7bf0a8
Ajithkumar Shah <ajithkumar.shah@m2pfintech.com>
2023-05-21 11:55:49 +0530

    hard cord env

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID
+  apiKey: "AIzaSyD2sf1d0Ex6G6RW0m5cBgtaoMuSzRW1vLA",
+  authDomain: "todo-app-95c44.firebaseapp.com",
+  projectId:  "todo-app-95c44",
+  storageBucket: "todo-app-95c44.appspot.com",
+  messagingSenderId: "1098593802143",
+  appId: "1:1098593802143:web:00ab0381728c28cd1b8faa"
 };
 
 // Initialize Firebase
